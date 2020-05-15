dafd899491b37a94a602a6dea031d42c4cf6fc17
nobobobo <ianhsu1221@gmail.com>
2020-05-14 20:36:52 -0700

    fix liker feature to have random chance to swipe left

--- autoswiper.py
+++ autoswiper.py
@@ -7,9 +7,18 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import ElementClickInterceptedException
 
 def pauseRandomTime(a=1, b=3):
+    '''
+    pause browser action for random time
+    '''
     sleep(random.randint(1, 3))
 
 def faceBookLogIn(email, password):
+    '''
+    faceBookLogIn: 
+    Search for email and pass input, and send key to those field
+    Then click anywhere else to able login click button. 
+    After that just click the button to login
+    '''
     driver.find_element_by_name('email').send_keys(email)
     pauseRandomTime()
     driver.find_element_by_name('pass').send_keys(password)
@@ -33,15 +42,19 @@ if __name__ == '__main__':
 
     sleep(5)
 
+    # sometimes log in UI for Tinder doesn't show Facebook login, instead 'More Options'
+
     try:
         driver.find_element_by_xpath('//button[text()="More Options"]').click()
         pauseRandomTime()
     except NoSuchElementException:
         pass
 
+    # click 'Log in with Facebook
     driver.find_element_by_xpath(
         '//button[@aria-label="Log in with Facebook"]').click()
 
+    # After five-second pause, switch handling page to login_page
     sleep(5)
 
     for handle in driver.window_handles:
@@ -50,22 +63,31 @@ if __name__ == '__main__':
 
     driver.switch_to.window(login_page)
 
+    # Log into FB and switch back the handling window
     faceBookLogIn(email, password)
 
     driver.switch_to.window(main_page)
 
-    pauseRandomTime()
+    # handle popup after initial login
+    sleep(5)
     driver.find_element_by_xpath('//span[text()="Allow"]').click()
     pauseRandomTime()
     driver.find_element_by_xpath('//span[text()="Not interested"]').click()
     pauseRandomTime(8,10)
 
 
-    for i in range(0, 100):
+    # Keep swiping right 1000 times, with 20% chance that swipes left
+    for i in range(0, 1000):
         try:
-            driver.find_element_by_xpath('//button[@aria-label="Like"]').click()
-            sleep(0.3)
+            # control random swipe-left: Current 20%
+            if random.randint(0, 4) == 0 :
+                driver.find_element_by_xpath('//button[@aria-label="Nope"]').click()
+            else :
+                driver.find_element_by_xpath('//button[@aria-label="Like"]').click()
 
+            sleep(0.2)
+
+        # Handling some pop up that blocks clicking on the button
         except ElementClickInterceptedException:
             print(i,' Log: ElementClickInterceptedException')
             try:
